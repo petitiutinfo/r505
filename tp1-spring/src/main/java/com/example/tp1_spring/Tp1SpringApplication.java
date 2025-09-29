27fb99515e89e45bc7fac1f23f6e91446049978a
Enzo Deher <enzo.deher@etu.iut-tlse3.fr>
2025-09-29 10:59:13 +0200

    Commit du TP1 avec un 'bonjour le monde' sur localhost:8080/bonjour

---
+++ tp1-spring/src/main/java/com/example/tp1_spring/Tp1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1SpringApplication.class, args);
+	}
+
+}
---
+++ tp1-spring/src/test/java/com/example/tp1_spring/Tp1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
